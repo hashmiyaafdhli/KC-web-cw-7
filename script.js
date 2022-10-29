e0b81c6be7fd0d1273f7cad4460cb3a5fe2cdddc
hash <hashmiyaafdhli03@gmail.com>
2022-10-29 19:37:14 +0300

    classwork 7 complete

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+function BMI(weight,height) {
+    let result = weight / (height * height);
+    return result;
+}
+console.log(BMI(80,1.5));
+
+function status(BodyMass) {
+    if (BodyMass < 18.5) {
+        return "لديك نقص في الوزن"
+    } else if (BodyMass < 25 && BodyMass > 18.5) {
+        return "وزنك صحي";
+    } else if (BodyMass > 25) {
+        return "لديك زيادة في الوزن"
+    }
+}
+
+function calculate() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+    let bmi_value = BMI(weight, height);
+    let desc = status(bmi_value);
+
+    let box = document.getElementById("result");
+    box.innerText = bmi_value + " == " + desc;
+}
+
